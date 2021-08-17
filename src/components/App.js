11ae2f9227ba00a52db6bd1a4b45aeb53ddc8258
coralie delpha <coralie.delpha@gmail.com>
2021-08-17 15:39:58 +0200

    bi-directional data flow and live state editing

--- src/components/App.js
+++ src/components/App.js
@@ -27,6 +27,14 @@ class App extends React.Component {
       });
   };
 
+  updateFish = (key, updatedFish) => {
+    // 1. Take a copy of the current state
+    const fishes = { ...this.state.fishes };
+    // 2. Update that state
+    fishes[key] = updatedFish;
+    // 3. Set that to state
+    this.setState({fishes: fishes});
+  }
   loadSampleFishes = () => {
     this.setState({ fishes: sampleFishes });
   };
@@ -79,7 +87,9 @@ class App extends React.Component {
         
         <Inventory
           addFish={this.addFish}
+          updateFish={this.updateFish}
           loadSampleFishes={this.loadSampleFishes}
+          fishes={this.state.fishes}
         />
       </div>
     );
